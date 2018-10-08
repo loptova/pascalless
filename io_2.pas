4af5638f647cf0510fd8186c5a00c70d9e6f295d
Volodya <johnlevis199@gmail.com>
2018-10-08 19:26:09 +0300

    второе задание

---
+++ io_2.pas
@@ -0,0 +1,11 @@
+﻿program io_2;
+var a,b:integer;
+begin
+writeln('введите первое число');
+readln(a);
+writeln('введите второе число');
+readln(b);
+writeln(a,' + ' ,b, ' = ' ,a+b);
+writeln(a,' - ' ,b, ' = ' ,a-b);
+writeln(a,' * ' ,b, ' = ' ,a*b);
+end.
