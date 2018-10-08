31ac80ed2f744aea4ac535d6a29b37695656a1f2
Volodya <johnlevis199@gmail.com>
2018-10-08 19:19:01 +0300

    первое задание

---
+++ io_1.pas
@@ -0,0 +1,11 @@
+﻿program io_1;
+var a:string;
+b:integer;
+begin
+writeln('Привет');
+writeln('Как тебя зовут?');
+readln(a);
+writeln('Сколько тебе лет?');
+readln(b);
+writeln('Привет ',a,'! Тебе ', b, ' лет' );
+end.
